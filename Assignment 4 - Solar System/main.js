aa553687eacb36d2a8873ffb9d6c83bd971c4600
Jacob Duncan <jacoblewisduncan@gmail.com>
2022-03-17 15:26:23 -0700

    Added initial properties of the Sun, Earth, and Moon.

--- Assignment 4 - Solar System/main.js
+++ Assignment 4 - Solar System/main.js
@@ -2,6 +2,10 @@
 "use strict";
 
 var gl;
+var sun = undefined;
+var earth = undefined;
+var moon = undefined;
+var t = 0.0;
 
 function init() {
     var canvas = document.getElementById("webgl-canvas");
@@ -11,6 +15,27 @@ function init() {
     gl.enable(gl.DEPTH_TEST);
    
     // Add your sphere creation and configuration code here
+	sun = new Sphere();
+	earth = new Sphere();
+	moon = new Sphere();
+	
+	// Radius
+	sun.radius = 12;
+	earth.radius = 6;
+	moon.radius = 2;
+	
+	// Oribit
+	earth.oribit = 10;
+	moon.orbit = 4;
+	
+	// Color
+	sun.color = vec4(1.0, 1.0, 0.0, 1.0);
+	earth.color = vec4(0.0, 0.0, 1.0, 1.0);
+	moon.color = vec4(0.5, 0.5, 0.5, 1.0);
+	
+	sun.P = P;
+	earth.P = P;
+	moon.P = P;
 
     requestAnimationFrame(render);
 }

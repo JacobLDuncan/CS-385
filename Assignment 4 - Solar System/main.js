b05ede675970911fa264c78fe84266c39981d21e
Jacob Duncan <jacoblewisduncan@gmail.com>
2022-03-17 19:08:19 -0700

    Continued transformation & animation work. Sun, Earth, & Moon are all visable with orbits.

--- Assignment 4 - Solar System/main.js
+++ Assignment 4 - Solar System/main.js
@@ -20,35 +20,89 @@ function init() {
 	moon = new Sphere();
 	
 	// Radius
-	sun.radius = 12;
-	earth.radius = 6;
-	moon.radius = 2;
+	sun.radius = 30;
+	earth.radius = 3;
+	moon.radius = 1;
 	
-	// Oribit
-	earth.oribit = 10;
-	moon.orbit = 4;
+	// Orbit
+	earth.orbit = 10 + 2 * sun.radius;
+	moon.orbit = 5 + 2 * earth.radius;
 	
 	// Color
 	sun.color = vec4(1.0, 1.0, 0.0, 1.0);
 	earth.color = vec4(0.0, 0.0, 1.0, 1.0);
 	moon.color = vec4(0.5, 0.5, 0.5, 1.0);
 	
+	// Overall Diameter
+	var D = 2 * (earth.orbit + moon.orbit + moon.radius);
+	
+	// Near & Far Parameters
+	var near = 8;
+	var far = near + D;
+	
+	// Fovy Parameters
+	var theta = (Math.asin((D/2) / (near + (D/2))) * (180/Math.PI));
+	var fovy = 2 * theta;
+	
+	// Aspect Perameters
+	var aspect = canvas.clientWidth/canvas.clientHeight;
+	
+	// Perspective Transformation
+	var P = perspective(fovy, aspect, near, far);
+	
 	sun.P = P;
 	earth.P = P;
 	moon.P = P;
+	
+	requestAnimationFrame(render);
 
-    requestAnimationFrame(render);
+	function render() {
+
+		// Update your motion variables here
+		const HoursPerDay = 24;
+		const HoursPerYear = 365.25 * HoursPerDay;
+		t += 1;
+		var year = t / 12;
+		var day = t / HoursPerYear * 360;
+		var hour = t % 360;
+
+		gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+		
+		// Add your rendering sequence here
+		var ms = new MatrixStack();
+		
+		// Viewing Transform (from Class 6)
+		var V = translate(0.0, 0.0, (-1/2*(near + far)));
+		ms.load(V);
+		
+		// Rendering the Sun (from Class 11)
+		ms.push();
+		ms.scale(sun.radius);
+		sun.MV = ms.current();
+		sun.render();
+		ms.pop();
+
+		// Rendering the Earth (from Class 11)
+		ms.push();
+		ms.rotate(year, vec3(0, 0, 1));
+		ms.translate(earth.orbit, 0, 0);
+		ms.push();
+		ms.rotate(day, vec3(0, 0, 1));
+		ms.scale(earth.radius);
+		earth.MV = ms.current();
+		earth.render();
+		ms.pop();
+
+		// Rendering the Moon (from Class 11)
+		ms.translate(moon.orbit, 0, 0);
+		ms.scale(moon.radius);
+		moon.MV = ms.current();
+		moon.render();
+		ms.pop();
+
+		requestAnimationFrame(render);
+	}
 }
 
-function render() {
-
-    // Update your motion variables here
-
-    gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    
-    // Add your rendering sequence here
-
-    requestAnimationFrame(render);
-}
 
 window.onload = init;
